bb1d197e1c30571118e7a959fc2d8b40ab398c4c
korobko <korobko@tspu.edu.ru>
2023-07-24 15:08:28 +0700

    load page length to document list

--- static/js/paginate_table.js
+++ static/js/paginate_table.js
@@ -1,13 +1,17 @@
 jQuery(document).ready(function () {
+    console.log('23');
     $tables = new DataTable('table[data-table-type^=paginate-table-]', {
-        dom: 't<"d-flex justify-content-center mt-2"p>',
+        dom: 'lt<"d-flex justify-content-center mt-2"p>',
         ordering: false,
-        pageLength: 9,
+        pageLength: 10,
+        sLengthMenu: 'Количество элементов',
+        lengthMenu: [[10, 20, 30, 40, -1], [10, 20, 30, 40, 'Все']],
         language: {
             oPaginate: {
                 sNext: '<div class="d-flex align-content-center justify-content-center"><span class="svg-arrow i-left"></span></div>',
                 sPrevious: '<div class="d-flex align-content-center justify-content-center"><span class="svg-arrow i-right"></span></div>',
-            }
+            },
+            lengthMenu: "Количество элементов _MENU_"
         }
     });
     console.log($tables);
