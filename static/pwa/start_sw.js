00b7cee87ec32c8091598a7433dc8eff2aa9e280
Andrey Korobko <korobko@tspu.edu.ru>
2024-03-05 21:37:26 +0700

    add comments to pwa files

--- static/pwa/start_sw.js
+++ static/pwa/start_sw.js
@@ -1,3 +1,7 @@
+// Файл подключается в теги head html-страницы.
+// Он нужен, чтобы запустить работу Service Worker и проверить её поддержку у клиента.
+
+// Функция проверки, доступна ли работа Service Worker у клиента. Доступность зависит от версии браузера или OC.
 function isSupported() {
     if (!('serviceWorker' in navigator)) {
         return;
@@ -5,6 +9,7 @@ function isSupported() {
     return true;
 }
 
+// Функция регистрирует Service Worker. В случае успешной регистрации или ошибки выводим лог в консоль.
 function registerSW() {
     navigator.serviceWorker.register('/static/pwa/serviceworker.js').then(
         (registration) => {
@@ -18,6 +23,7 @@ function registerSW() {
     return true;
 }
 
+// Запуск Service Worker происходит после загрузки всей страницы событием 'load'.
 window.addEventListener('load', function () {
     const supported = isSupported();
     if (supported) {
